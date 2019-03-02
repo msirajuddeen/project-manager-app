5abe80db74c835abd62367c98dc3c9e7cc4ec8ae
msirajuddeen <46441835+msirajuddeen@users.noreply.github.com>
2019-03-02 17:23:11 +0530

    user component changes

--- project-manager-ui/src/main/web/src/app/app-routing.module.ts
+++ project-manager-ui/src/main/web/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectComponent } from './pages/project/project.component';
+import { UserComponent } from './pages/user/user.component';
+import { AddTaskComponent } from './pages/task/add-task/add-task.component';
+import { ViewTaskComponent } from './pages/task/view-task/view-task.component';
+import { EditTaskComponent } from './pages/task/edit-task/edit-task.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+    imports: [
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/addUser', pathMatch: 'full' },
+      { path: 'addUser', component: UserComponent },
+      { path: 'addProject', component: ProjectComponent },
+      { path: 'addTask', component: AddTaskComponent  },
+      { path: 'editTask', component: EditTaskComponent },
+      { path: 'viewTask', component: ViewTaskComponent  }
+    ])
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- project-manager-ui/src/main/web/src/app/app.module.ts
+++ project-manager-ui/src/main/web/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProjectComponent } from './pages/project/project.component';
+import { UserComponent } from './pages/user/user.component';
+import { AddTaskComponent } from './pages/task/add-task/add-task.component';
+import { ViewTaskComponent } from './pages/task/view-task/view-task.component';
+import { EditTaskComponent } from './pages/task/edit-task/edit-task.component';
+import { ProjectManagerService } from './shared/project-manager-service';
+import { SortFilterPipe } from './shared/sort-pipe';
+import { SearchFilter } from './shared/search-filter';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    UserComponent,
+    AddTaskComponent,
+    ViewTaskComponent,
+    EditTaskComponent,
+    SortFilterPipe,
+    SearchFilter
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProjectManagerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
